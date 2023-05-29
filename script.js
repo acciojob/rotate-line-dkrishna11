c6bc7a5927399a66757d51dc0ab739443212ce79
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-29 14:46:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+let dynamicStyles = null;
+
+function addAnimation(body) {
+  if (!dynamicStyles) {
+    dynamicStyles = document.createElement('style');
+    dynamicStyles.type = 'text/css';
+    document.head.appendChild(dynamicStyles);
+  }
+
+  dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
+}
+
+addAnimation(`
+      @keyframes move-eye { 
+         from {
+          transform: translateX(-50%) rotate(0deg);
+         }
+        to {
+           transform: translateX(-50%) rotate(360deg);
+        }
+      }
+    `);
+
+
+
+var element = document.createElement("div");
+element.className = "cylon-eye";
+element.style.height = "2px";
+element.style.width = "200px";
+element.style.backgroundColor = "black";
+element.style.animation = "4s linear 0s infinite alternate move-eye";
+element.style.position="absolute";
+element.style.top="50%";
+element.style.left="50%";
+
+document.body.appendChild(element);
